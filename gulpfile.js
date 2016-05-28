9cd3a964365bd738544c97470c2be8ad60cd0d83
jyhuang <jyhuang422@gmail.com>
2016-05-28 22:49:40 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var buffer = require('vinyl-buffer');
+var imagemin = require('gulp-imagemin');
+
+var spritesmith = require('gulp.spritesmith');
+
+gulp.task('default', ['sprite'], function() {
+  // place code for your default task here
+});
+
+gulp.task('sprite', function () {
+  // Generate our spritesheet
+  var spriteData = gulp.src('icon/*.png').pipe(spritesmith({
+    algorithm: 'binary-tree',
+    //padding: 2,
+    imgName: 'sprite/demo_sprite.png',
+    cssName: 'assets/less/demo_sprite.less'
+  }));
+
+  var cssStream = spriteData.css
+    .pipe(gulp.dest(''));
+
+  // Pipe image stream through image optimizer and onto disk
+  var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+    .pipe(buffer())
+    .pipe(imagemin())
+    .pipe(gulp.dest(''));
+
+});
